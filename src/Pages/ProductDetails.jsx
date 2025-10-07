13f69de742760738da9a265a9a0b5ca0e4607ce0
AshiqurRahmanAshik <ashiqurrahmanashik111@gmail.com>
2025-10-07 15:27:28 +0600

    Wishlist Added

--- src/Pages/ProductDetails.jsx
+++ src/Pages/ProductDetails.jsx
@@ -1,28 +1,50 @@
 import React from "react";
 import { useParams } from "react-router";
 import useProducts from "../Hooks/useProducts";
+import Wishlist from "./Wishlist";
 
 const ProductDetails = () => {
   const { id } = useParams();
-  const { products, error, loading } = useProducts();
+  const { products, loading } = useProducts();
   const product = products.find((p) => String(p.id) === id);
   if (loading) return <p>Loading...</p>;
   const { image, name, description, category, price } = product;
+
+  const handleAddToWishList = () => {
+    const existingList = JSON.parse(localStorage.getItem("wishlist"));
+    let updatedList = [];
+    if (existingList) {
+      const isDuplicate = existingList.some((p) => p.id === product.id);
+      if (isDuplicate) return alert("Already Added");
+      updatedList = [...existingList, product];
+    } else {
+      updatedList.push(product);
+    }
+    localStorage.setItem("wishlist", JSON.stringify(updatedList));
+  };
+
   return (
-    <div className="card bg-base-100 border shadow-sm">
-      <figure className="h-84 overflow-hidden">
-        <img className="w-full object-cover" src={image} alt="furniture" />
-      </figure>
-      <div className="card-body"> 
-        <h2 className="card-title">{name}</h2>
-        <p>{description}</p>
-        <p>Category: {category}</p>
-        <p>Price: ${price}</p>
-        <div className="card-actions justify-end">
-          <button className="btn btn-outline">Add to Wishlist</button>
+    <>
+      <h2 className="text-center text-2xl font-semibold pb-5">
+        Product Details
+      </h2>
+      <div className="card bg-base-100 border shadow-sm">
+        <figure className="h-84 overflow-hidden">
+          <img className="w-full object-cover" src={image} alt="furniture" />
+        </figure>
+        <div className="card-body">
+          <h2 className="card-title">{name}</h2>
+          <p>{description}</p>
+          <p>Category: {category}</p>
+          <p>Price: ${price}</p>
+          <div className="card-actions justify-end">
+            <button onClick={handleAddToWishList} className="btn btn-outline">
+              Add to Wishlist
+            </button>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
--- src/Pages/Wishlist.jsx
+++ src/Pages/Wishlist.jsx
@@ -1,11 +1,79 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
 
 const Wishlist = () => {
-    return (
-        <div>
-            Wishlist
-        </div>
-    );
+  const [wishlist, setWishList] = useState([]);
+  const [sortOrder, setSortOrder] = useState("none");
+  useEffect(() => {
+    const saveList = JSON.parse(localStorage.getItem("wishlist"));
+    if (saveList) setWishList(saveList);
+  }, []);
+
+  const sortedItem = (() => {
+    if (sortOrder === "price-asc") {
+      return [...wishlist].sort((a, b) => a.price - b.price);
+    } else if (sortOrder === "price-dsc") {
+      return [...wishlist].sort((a, b) => b.price - a.price);
+    } else {
+      return wishlist;
+    }
+  })();
+  const handleRemove = (id) => {
+    const existingList = JSON.parse(localStorage.getItem("wishlist"));
+    const updatedList = existingList.filter((p) => p.id != id);
+    setWishList(updatedList);
+  };
+  return (
+    <div>
+      <div className="flex justify-between py-5 items-center">
+        <h2 className="text-xl md:text-3xl font-bold">
+          Wishlists <br />
+          <span className="text-sm text-gray-700">
+            ({wishlist.length} Products Found)
+          </span>
+        </h2>
+        <label className="form-control  max-w-xs">
+          <select
+            className="select select-bordered"
+            value={sortOrder}
+            onChange={(e) => setSortOrder(e.target.value)}
+          >
+            <option value="none">Sort By Price</option>
+            <option value="price-asc">Low-&gt; High</option>
+            <option value="price-dsc">High-&gt; Low</option>
+          </select>
+        </label>
+      </div>
+      <div className="space-y-5">
+        {sortedItem.map((p) => (
+          <div
+            key={p.id}
+            className="card card-side bg-base-100 shadow-sm border items-center p-5"
+          >
+            <figure>
+              <img
+                className="w-40 h-28 object-cover"
+                src={p.image}
+                alt={p.name}
+              />
+            </figure>
+            <div className="card-body">
+              <h2 className="card-title">{p.name}</h2>
+              <p>{p.category}</p>
+              <p>{p.price}$</p>
+            </div>
+            <div className="card-actions justify-end">
+              <button
+                onClick={() => handleRemove(p.id)}
+                className="btn btn-outline"
+              >
+                Remove
+              </button>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Wishlist;
